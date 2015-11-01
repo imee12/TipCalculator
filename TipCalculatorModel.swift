40a67d4d333342d8362a6c354c56162c1a58b8f7
Imee Cuison <imee@dynepic.com>
2015-11-01 03:33:21 -0500

    first commit

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,44 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Imee Cuison on 11/1/15.
+//  Copyright © 2015 Imee Cuison. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+    
+    
+}
+
+
